53d4a761224470842cd57f414d3b19f4674b172f
Alireza Asadi <asady529@gmail.com>
2024-01-10 02:08:39 +0330

    Change sqlalchemy engine.execute() to newer connection.execute()

--- KGTorrent/db_communication_handler.py
+++ KGTorrent/db_communication_handler.py
@@ -467,7 +467,8 @@ class DbCommunicationHandler:
 
             print('Executing "{}"'.format(query))
             try:
-                self._engine.execute(query)
+                with self._engine.connect() as conn:
+                    result = conn.execute(query)
             except IntegrityError as e:
                 print("\t - INTEGRITY ERROR. Can't update table ", table_name, file=sys.stderr)
 
